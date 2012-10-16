4e03f4d071df227d6c2ebf9a971636ad69012d37
Cash <kusoko@gmail.com>
2012-10-16 14:50:46 +0800

    MPP_HW_01

--- calculator/usokoViewController.h
+++ calculator/usokoViewController.h
@@ -8,6 +8,37 @@
 
 #import <UIKit/UIKit.h>
 
-@interface usokoViewController : UIViewController
+@interface usokoViewController : UIViewController {
+    NSInteger binaryOperator;
+    //0:noop 1:+ 2:- 3:* 4:/ can use enum
+    double currentResult;
+    double firstOperand;
+    bool dotFlag;
+    bool negativeFlag;
+    bool firstOperandAssigned;
+    NSInteger negativeExponent;
+}
+
+@property (strong, nonatomic) IBOutlet UILabel *display;
+
+- (IBAction)number0Button:(id)sender;
+- (IBAction)number1Button:(id)sender;
+- (IBAction)number2Button:(id)sender;
+- (IBAction)number3Button:(id)sender;
+- (IBAction)number4Button:(id)sender;
+- (IBAction)number5Button:(id)sender;
+- (IBAction)number6Button:(id)sender;
+- (IBAction)number7Button:(id)sender;
+- (IBAction)number8Button:(id)sender;
+- (IBAction)number9Button:(id)sender;
+- (IBAction)dotButton:(id)sender;
+- (IBAction)plusSignButton:(id)sender;
+- (IBAction)minusSignButton:(id)sender;
+- (IBAction)multiplySignButton:(id)sender;
+- (IBAction)divideSignButton:(id)sender;
+- (IBAction)squareSignButton:(id)sender;
+- (IBAction)backSpaceButton:(id)sender;
+- (IBAction)clearButton:(id)sender;
+- (IBAction)evaluationButton:(id)sender;
 
 @end
--- calculator/usokoViewController.m
+++ calculator/usokoViewController.m
@@ -14,16 +14,366 @@
 
 @implementation usokoViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+- (IBAction)clearButton:(id)sender {
+    currentResult = 0;
+    firstOperand = 0;
+    negativeExponent = 0;
+    binaryOperator = 0;
+    dotFlag = NO;
+    firstOperandAssigned = NO;
+    self.display.text = [@(currentResult) stringValue];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//good habit
+
+#pragma mark -
+#pragma mark numberPad
+
+- (IBAction)number0Button:(id)sender {
+    if(!dotFlag)
+        currentResult *= 10;
+    else
+        negativeExponent++;
+    self.display.text = [@(currentResult) stringValue];
+}
+
+- (IBAction)number1Button:(id)sender {
+    if(!dotFlag) {
+        currentResult *= 10;
+        if(!negativeFlag)
+            currentResult += 1;
+        else
+            currentResult -= 1;
+    }
+    else {
+        if(!negativeFlag)
+            currentResult += 1*pow(0.1, negativeExponent+1);
+        else
+            currentResult -= 1*pow(0.1, negativeExponent+1);
+        negativeExponent++;
+    }
+    
+    self.display.text = [@(currentResult) stringValue];
+}
+- (IBAction)number2Button:(id)sender {
+    if(!dotFlag) {
+        currentResult *= 10;
+        if(!negativeFlag)
+            currentResult += 2;
+        else
+            currentResult -= 2;
+    }
+    else {
+        if(!negativeFlag)
+            currentResult += 2*pow(0.1, negativeExponent+1);
+        else
+            currentResult -= 2*pow(0.1, negativeExponent+1);
+        negativeExponent++;
+    }
+    self.display.text = [@(currentResult) stringValue];
+}
+- (IBAction)number3Button:(id)sender {
+    if(!dotFlag) {
+        currentResult *= 10;
+        if(!negativeFlag)
+            currentResult += 3;
+        else
+            currentResult -= 3;
+    }
+    else {
+        if(!negativeFlag)
+            currentResult += 3*pow(0.1, negativeExponent+1);
+        else
+            currentResult -= 3*pow(0.1, negativeExponent+1);
+        negativeExponent++;
+    }
+    self.display.text = [@(currentResult) stringValue];
+}
+- (IBAction)number4Button:(id)sender {
+    if(!dotFlag) {
+        currentResult *= 10;
+        if(!negativeFlag)
+            currentResult += 4;
+        else
+            currentResult -= 4;
+    }
+    else {
+        if(!negativeFlag)
+            currentResult += 4*pow(0.1, negativeExponent+1);
+        else
+            currentResult -= 4*pow(0.1, negativeExponent+1);
+        negativeExponent++;
+    }
+    self.display.text = [@(currentResult) stringValue];
+}
+- (IBAction)number5Button:(id)sender {
+    if(!dotFlag) {
+        currentResult *= 10;
+        if(!negativeFlag)
+            currentResult += 5;
+        else
+            currentResult -= 5;
+    }
+    else {
+        if(!negativeFlag)
+            currentResult += 5*pow(0.1, negativeExponent+1);
+        else
+            currentResult -= 5*pow(0.1, negativeExponent+1);
+        negativeExponent++;
+    }
+    self.display.text = [@(currentResult) stringValue];
+}
+- (IBAction)number6Button:(id)sender {
+    if(!dotFlag) {
+        currentResult *= 10;
+        if(!negativeFlag)
+            currentResult += 6;
+        else
+            currentResult -= 6;
+    }
+    else {
+        if(!negativeFlag)
+            currentResult += 6*pow(0.1, negativeExponent+1);
+        else
+            currentResult -= 6*pow(0.1, negativeExponent+1);
+        negativeExponent++;
+    }
+    self.display.text = [@(currentResult) stringValue];
+}
+- (IBAction)number7Button:(id)sender {
+    if(!dotFlag) {
+        currentResult *= 10;
+        if(!negativeFlag)
+            currentResult += 7;
+        else
+            currentResult -= 7;
+    }
+    else {
+        if(!negativeFlag)
+            currentResult += 7*pow(0.1, negativeExponent+1);
+        else
+            currentResult -= 7*pow(0.1, negativeExponent+1);
+        negativeExponent++;
+    }
+    self.display.text = [@(currentResult) stringValue];
+}
+- (IBAction)number8Button:(id)sender {
+    if(!dotFlag) {
+        currentResult *= 10;
+        if(!negativeFlag)
+            currentResult += 8;
+        else
+            currentResult -= 8;
+    }
+    else {
+        if(!negativeFlag)
+            currentResult += 8*pow(0.1, negativeExponent+1);
+        else
+            currentResult -= 8*pow(0.1, negativeExponent+1);
+        negativeExponent++;
+    }
+    self.display.text = [@(currentResult) stringValue];
+}
+- (IBAction)number9Button:(id)sender {
+    if(!dotFlag) {
+        currentResult *= 10;
+        if(!negativeFlag)
+            currentResult += 9;
+        else
+            currentResult -= 9;
+    }
+    else {
+        if(!negativeFlag)
+            currentResult += 9*pow(0.1, negativeExponent+1);
+        else
+            currentResult -= 9*pow(0.1, negativeExponent+1);
+        negativeExponent++;
+    }
+    self.display.text = [@(currentResult) stringValue];
+}
+
+#pragma mark -
+#pragma mark signButton
+
+- (IBAction)dotButton:(id)sender {
+    dotFlag = YES;
+}
+- (IBAction)plusSignButton:(id)sender {
+    switch (binaryOperator) {
+        case 1:
+            currentResult += firstOperand;
+            self.display.text = [@(currentResult) stringValue];
+            break;
+        case 2:
+            currentResult = firstOperand-currentResult;
+            self.display.text = [@(currentResult) stringValue];
+            break;
+        case 3:
+            currentResult *= firstOperand;
+            self.display.text = [@(currentResult) stringValue];
+            break;
+        case 4:
+            if(currentResult!=0) {
+                currentResult = firstOperand/currentResult;
+                self.display.text = [@(currentResult) stringValue];
+            }
+            else
+                self.display.text = @"FOOL! DON'T DIVIDE BY ZERO!";
+            break;
+        default:
+            break;
+    }
+    negativeFlag = NO;
+    binaryOperator = 1;
+    firstOperand = currentResult;
+    currentResult = 0;
+    dotFlag = NO;
+    negativeExponent = 0;
+}
+- (IBAction)minusSignButton:(id)sender {
+    if(currentResult == 0 && !firstOperandAssigned)
+        negativeFlag = YES;
+    else {
+        switch (binaryOperator) {
+            case 1:
+                currentResult += firstOperand;
+                self.display.text = [@(currentResult) stringValue];
+                break;
+            case 2:
+                currentResult = firstOperand-currentResult;
+                self.display.text = [@(currentResult) stringValue];
+                break;
+            case 3:
+                currentResult *= firstOperand;
+                self.display.text = [@(currentResult) stringValue];
+                break;
+            case 4:
+                if(currentResult!=0) {
+                    currentResult = firstOperand/currentResult;
+                    self.display.text = [@(currentResult) stringValue];
+                }
+                else
+                    self.display.text = @"FOOL! DON'T DIVIDE BY ZERO!";
+                break;
+            default:
+                break;
+        }
+        negativeFlag = NO;
+        binaryOperator = 2;
+        firstOperand = currentResult;
+        currentResult = 0;
+        dotFlag = NO;
+        negativeExponent = 0;
+    }
+}
+- (IBAction)multiplySignButton:(id)sender {
+    switch (binaryOperator) {
+        case 1:
+            currentResult += firstOperand;
+            self.display.text = [@(currentResult) stringValue];
+            break;
+        case 2:
+            currentResult = firstOperand-currentResult;
+            self.display.text = [@(currentResult) stringValue];
+            break;
+        case 3:
+            currentResult *= firstOperand;
+            self.display.text = [@(currentResult) stringValue];
+            break;
+        case 4:
+            if(currentResult!=0) {
+                currentResult = firstOperand/currentResult;
+                self.display.text = [@(currentResult) stringValue];
+            }
+            else
+                self.display.text = @"FOOL! DON'T DIVIDE BY ZERO!";
+            break;
+        default:
+            break;
+    }
+    negativeFlag = NO;
+    binaryOperator = 3;
+    firstOperand = currentResult;
+    currentResult = 0;
+    dotFlag = NO;
+    negativeExponent = 0;
+}
+- (IBAction)divideSignButton:(id)sender {
+    switch (binaryOperator) {
+        case 1:
+            currentResult += firstOperand;
+            self.display.text = [@(currentResult) stringValue];
+            break;
+        case 2:
+            currentResult = firstOperand-currentResult;
+            self.display.text = [@(currentResult) stringValue];
+            break;
+        case 3:
+            currentResult *= firstOperand;
+            self.display.text = [@(currentResult) stringValue];
+            break;
+        case 4:
+            if(currentResult!=0) {
+                currentResult = firstOperand/currentResult;
+                self.display.text = [@(currentResult) stringValue];
+            }
+            else
+                self.display.text = @"FOOL! DON'T DIVIDE BY ZERO!";
+            break;
+        default:
+            break;
+    }
+    negativeFlag = NO;
+    binaryOperator = 4;
+    firstOperand = currentResult;
+    currentResult = 0;
+    dotFlag = NO;
+    negativeExponent = 0;
+}
+- (IBAction)squareSignButton:(id)sender {
+    currentResult = sqrt(currentResult);
+    binaryOperator = 0;
+    dotFlag = NO;
+    negativeFlag = NO;
+    firstOperand = 0;
+    self.display.text = [@(currentResult) stringValue];
+}
+- (IBAction)backSpaceButton:(id)sender {
+    
+}
+
+- (IBAction)evaluationButton:(id)sender {
+    switch (binaryOperator) {
+        case 1:
+            currentResult += firstOperand;
+            self.display.text = [@(currentResult) stringValue];
+            break;
+        case 2:
+            currentResult = firstOperand-currentResult;
+            self.display.text = [@(currentResult) stringValue];
+            break;
+        case 3:
+            currentResult *= firstOperand;
+            self.display.text = [@(currentResult) stringValue];
+            break;
+        case 4:
+            if(currentResult!=0) {
+                currentResult = firstOperand/currentResult;
+                self.display.text = [@(currentResult) stringValue];
+            }
+            else
+                self.display.text = @"FOOL! DON'T DIVIDE BY ZERO!";
+            break;
+        default:
+            break;
+    }
+    
+    currentResult = 0;
+    binaryOperator = 0;
+    dotFlag = NO;
+    negativeFlag = NO;
+    negativeExponent = 0;
 }
 
 @end
